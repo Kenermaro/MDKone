60902e63ce41bc970c47cc72a3c279fd4d0f0c03
Kenermaro <pkear98@bk.ru>
2023-09-13 15:42:50 +0000

    kirill

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -115,5 +115,69 @@ namespace WindowsFormsApp1
                 connection.Close();
             }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string dbfilePath = @"C:\4ekmaryov\AutoSerivirs\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\your_database.db";
+            string connectionString = $"Data Source={dbfilePath}; Version=3;";
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SQLiteCommand command = new SQLiteCommand("SELECT Image1, Image2, TextColumn FROM ImagesTable LIMIT 1 OFFSET 3;", connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            byte[] imagen1Bytes = (byte[])reader["Image1"];
+                            byte[] imagen2Bytes = (byte[])reader["Image2"];
+                            string textValue = reader["TextColumn"].ToString();
+                            using (MemoryStream stream1 = new MemoryStream(imagen1Bytes))
+                            using (MemoryStream stream2 = new MemoryStream(imagen2Bytes))
+                            {
+                                pictureBox1.Image = Image.FromStream(stream1);
+                                pictureBox2.Image = Image.FromStream(stream2);
+                            }
+                            label1.Text = textValue;
+                        }
+                    }
+                }
+                connection.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string dbfilePath = @"C:\4ekmaryov\AutoSerivirs\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\your_database.db";
+            string connectionString = $"Data Source={dbfilePath}; Version=3;";
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SQLiteCommand command = new SQLiteCommand("SELECT Image1, Image2, TextColumn FROM ImagesTable LIMIT 1 OFFSET 4;", connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            byte[] imagen1Bytes = (byte[])reader["Image1"];
+                            byte[] imagen2Bytes = (byte[])reader["Image2"];
+                            string textValue = reader["TextColumn"].ToString();
+                            using (MemoryStream stream1 = new MemoryStream(imagen1Bytes))
+                            using (MemoryStream stream2 = new MemoryStream(imagen2Bytes))
+                            {
+                                pictureBox1.Image = Image.FromStream(stream1);
+                                pictureBox2.Image = Image.FromStream(stream2);
+                            }
+                            label1.Text = textValue;
+                        }
+                    }
+                }
+                connection.Close();
+            }
+        }
     }
 }
